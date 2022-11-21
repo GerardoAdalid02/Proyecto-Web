6cf1bc9a7ad598761e9bb4d5e8b0fd44e27de56e
GerardoAdalid02 <mg1959012022@unab.edu.sv>
2022-11-20 19:37:08 -0600

    Adición de Template de HTML a Backend y app portfolio

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path ("", views.home, name="home"),
+    path ('', views.home, name="home"),
     path ('about/', views.about, name='about'),
     path ('portfolio/', views.portfolio, name='portfolio'),
     path ('contact/', views.contact, name='contact'),
